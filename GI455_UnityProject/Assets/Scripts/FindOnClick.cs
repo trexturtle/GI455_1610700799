bf1fd25dddcb083db2e690b7ade0f62937e09c0b
Santiphap Lumluar <trexturtleowen@gmail.com>
2021-01-29 08:49:34 +0700

    Send Assignment Week1

---
+++ GI455_UnityProject/Assets/Scripts/FindOnClick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindOnClick : MonoBehaviour
+{
+    public List<string> wordList;
+    public Text showWordList;
+    public Text searchResult;
+    public InputField searchBox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        string text = "";
+
+        for (int i = 0; i < wordList.Count; i++)
+        {
+            text += wordList[i] + "\n";
+        }
+
+        showWordList.text = text;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickResult()
+    {
+        if (wordList.Contains(searchBox.text))
+        {
+            searchResult.text = "[ <color=green>" + searchBox.text + "</color> ]" + " is Found.";
+        }
+        else
+        {
+            searchResult.text = "[ <color=red>" + searchBox.text + "</color> ]" + " is not Found.";
+        }
+    }
+}
